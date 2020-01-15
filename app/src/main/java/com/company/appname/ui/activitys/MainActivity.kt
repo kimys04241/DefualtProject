9f971426c5310e3da9744f23702b6926bb40b2ff
lyze-y <y@lyze.ai>
2020-01-15 16:22:30 +0900

    Initial commit

---
+++ app/src/main/java/com/company/appname/ui/activitys/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.company.appname.ui.activitys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.company.appname.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        
+    }
+}
